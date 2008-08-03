763ccd3c46a0c579393406833c80179687b7d4f5
e-yamane <e-yamane@32b2b61f-7f20-47f8-a8dc-572efa4bece0>
2008-08-03 14:06:58 +0000

    git-svn-id: https://dev.rough-diamond.jp/svn/framework/framework/trunk/framework@61 32b2b61f-7f20-47f8-a8dc-572efa4bece0

--- src/java/jp/rough_diamond/ant/taskdefs/HibernateDocletTaskExt.java
+++ src/java/jp/rough_diamond/ant/taskdefs/HibernateDocletTaskExt.java
@@ -1,13 +1,32 @@
 package jp.rough_diamond.ant.taskdefs;
 
+import java.io.File;
+import java.util.StringTokenizer;
+
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.types.FileSet;
 
 import xdoclet.modules.hibernate.HibernateCfgSubTask;
 import xdoclet.modules.hibernate.HibernateDocletTask;
 
 public class HibernateDocletTaskExt extends HibernateDocletTask {
+	public void setFileSetDirs(String filesetDirs) {
+		System.out.println("xyz:" + filesetDirs);
+		String pattern = getProject().getProperty("hibernate.pattern");
+		StringTokenizer tokenizer = new StringTokenizer(filesetDirs, ";");
+		while(tokenizer.hasMoreTokens()) {
+			String token = tokenizer.nextToken();
+			System.out.println(token);
+			FileSet fs = new FileSet();
+			fs.setDir(new File(token));
+			fs.setIncludes(pattern);
+			addFileset(fs);
+		}
+	}
+
 	@Override
     public Object createDynamicElement(String name) throws BuildException {
+		System.out.println("ZZZZZZZZZ:" + name);
 		if("hibernatecfg".equals(name)) {
 			HibernateCfgSubTask base = (HibernateCfgSubTask) super.createDynamicElement(name);
 			HibernateCfgSubTaskExt ext = new HibernateCfgSubTaskExt(base);
