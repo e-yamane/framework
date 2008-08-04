bf0f84c494ac3c9ee9a061b39a5523c3cef6123e
e-yamane <e-yamane@32b2b61f-7f20-47f8-a8dc-572efa4bece0>
2008-08-04 13:29:44 +0000

    git-svn-id: https://dev.rough-diamond.jp/svn/framework/framework/trunk/framework@67 32b2b61f-7f20-47f8-a8dc-572efa4bece0

--- src/java/org/dbunit/ant/Export4TorqueSchema.java
+++ src/java/org/dbunit/ant/Export4TorqueSchema.java
@@ -3,8 +3,10 @@ package org.dbunit.ant;
 import java.io.File;
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -43,6 +45,7 @@ public class Export4TorqueSchema extends Export {
 	}
 
 	private File baseDest;
+	private Set<String> exportedTables;
     public void execute(IDatabaseConnection connection) throws DatabaseUnitException {
     	baseDest = getDest();
     	try {
@@ -51,6 +54,7 @@ public class Export4TorqueSchema extends Export {
 			} else {
 				System.out.println(schema);
 				String[] schemaNames = getFileNames();
+				exportedTables = new HashSet<String>();
 				for(int i = 0 ; i < schemaNames.length ; i++) {
 					System.out.println("exporting..." + schemaNames[i]);
 					execute(connection, schemaNames[i]);
@@ -68,31 +72,43 @@ public class Export4TorqueSchema extends Export {
 			dbf.setValidating(false);
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			File f = new File(rootDir, schemaName);
-			Document doc = db.parse(f);
-			Element el = doc.getDocumentElement();
-			NodeList list = el.getChildNodes();
-			for(int i = 0 ; i < list.getLength() ; i++) {
-				Node node = (Node)list.item(i);
-				if(node instanceof Element) {
-					Element child = (Element)node;
-					if("table".equals(child.getNodeName()) && !Boolean.valueOf(child.getAttribute("skipSql")).booleanValue()) {
-                        
-						executeIt(connection, child.getAttribute("name").toUpperCase());
-					}
-				}
-			}
+			execute(connection, db, f);
 		} catch(Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException();
 		}
 	}
 
+	private void execute(IDatabaseConnection connection, DocumentBuilder db, File schemaFile) throws Exception {
+		Document doc = db.parse(schemaFile);
+		Element el = doc.getDocumentElement();
+		NodeList list = el.getChildNodes();
+		for(int i = 0 ; i < list.getLength() ; i++) {
+			Node node = (Node)list.item(i);
+			if(node instanceof Element) {
+				Element child = (Element)node;
+				String tagName = child.getNodeName();
+				if("external-schema".equals(tagName)) {
+					String fileName = child.getAttribute("filename");
+					File exportSchemaFile = new File(schemaFile.getParentFile(), fileName);
+					System.out.println(exportSchemaFile.getCanonicalPath());
+					execute(connection, db, exportSchemaFile);
+				} else if("table".equals(tagName) && !Boolean.valueOf(child.getAttribute("skipSql")).booleanValue()) {
+					executeIt(connection, child.getAttribute("name").toUpperCase());
+				}
+			}
+		}
+	}
+	
 	private List<Object> baseTables;
 	@Override
 	public List<Object> getTables() {
 		return super.getTables();
 	}
 	private void executeIt(IDatabaseConnection connection, String tableName) {
+		if(exportedTables.contains(tableName)) {
+			return;
+		}
 		baseTables = new ArrayList<Object>(getTables());
 		try {
 			cloneQueries(tableName);
@@ -105,6 +121,7 @@ public class Export4TorqueSchema extends Export {
 				setDest(new File(fileName));
 			}
 			super.execute(connection);
+			exportedTables.add(tableName);
 		} catch(Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
