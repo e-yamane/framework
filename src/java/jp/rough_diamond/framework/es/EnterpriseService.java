bc4ccd44a1a1f4824bee39e73dce2336af88c1ab
okumura <okumura@32b2b61f-7f20-47f8-a8dc-572efa4bece0>
2009-03-24 16:45:10 +0000

    * [ticket:7 $Dateキーワードの展開で文字化け]が発生していたため、不要な@autherおよび@dateを消す。  * [ticket:5 Copyright記述の埋め込み] framework/es 配下
    git-svn-id: https://dev.rough-diamond.jp/svn/framework/framework/trunk/framework@360 32b2b61f-7f20-47f8-a8dc-572efa4bece0

--- src/java/jp/rough_diamond/framework/es/EnterpriseService.java
+++ src/java/jp/rough_diamond/framework/es/EnterpriseService.java
@@ -1,10 +1,15 @@
+/*
+ * Copyright (c) 2008, 2009
+ *  Rough Diamond Co., Ltd.              -- http://www.rough-diamond.co.jp/
+ *  Information Systems Institute, Ltd.  -- http://www.isken.co.jp/
+ *  All rights reserved.
+ */
 package jp.rough_diamond.framework.es;
 
 import jp.rough_diamond.framework.service.Service;
 
 /**
  * エンタープライズサービス
- * @author e-yamane
  */
 public interface EnterpriseService extends Service {
 }
--- src/java/jp/rough_diamond/framework/es/ServiceConnecter.java
+++ src/java/jp/rough_diamond/framework/es/ServiceConnecter.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2008, 2009
+ *  Rough Diamond Co., Ltd.              -- http://www.rough-diamond.co.jp/
+ *  Information Systems Institute, Ltd.  -- http://www.isken.co.jp/
+ *  All rights reserved.
+ */
 package jp.rough_diamond.framework.es;
 
 import java.lang.annotation.ElementType;
@@ -7,7 +13,6 @@ import java.lang.annotation.Target;
 
 /**
  * エンタープライズサービスコネクター
- * @author e-yamane
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
