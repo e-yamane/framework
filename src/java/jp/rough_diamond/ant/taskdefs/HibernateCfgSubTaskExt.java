5cc4c420aa5df0a65421d9e8f19dcf968d1395e0
okumura <okumura@32b2b61f-7f20-47f8-a8dc-572efa4bece0>
2009-03-24 10:13:32 +0000

    Copyright記述の埋め込み
    git-svn-id: https://dev.rough-diamond.jp/svn/framework/framework/trunk/framework@341 32b2b61f-7f20-47f8-a8dc-572efa4bece0

--- src/java/jp/rough_diamond/ant/taskdefs/HibernateCfgSubTaskExt.java
+++ src/java/jp/rough_diamond/ant/taskdefs/HibernateCfgSubTaskExt.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2008, 2009
+ *  Rough Diamond Co., Ltd.              -- http://www.rough-diamond.co.jp/
+ *  Information Systems Institute, Ltd.  -- http://www.isken.co.jp/
+ *  All rights reserved.
+ */
 package jp.rough_diamond.ant.taskdefs;
 
 import java.util.Collection;
