7dd8f9ac39377129fa0509828dce9ba53008a7b6
e-yamane <e-yamane@32b2b61f-7f20-47f8-a8dc-572efa4bece0>
2009-03-26 05:40:32 +0000

    DIコンテナフレームワークを統合
    git-svn-id: https://dev.rough-diamond.jp/svn/framework/framework/trunk/framework@400 32b2b61f-7f20-47f8-a8dc-572efa4bece0

---
+++ src/test/jp/rough_diamond/commons/di/DIContainerFactoryTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2008, 2009
+ *  Rough Diamond Co., Ltd.              -- http://www.rough-diamond.co.jp/
+ *  Information Systems Institute, Ltd.  -- http://www.isken.co.jp/
+ *  All rights reserved.
+ */
+package jp.rough_diamond.commons.di;
+
+import junit.framework.TestCase;
+
+public class DIContainerFactoryTest extends TestCase {
+	public void testGetDefaultDIContainer() {
+		DIContainer container = DIContainerFactory.getDIContainer();
+		assertEquals("class unmuch.", container.getClass(), SpringFramework.class);
+	}
+
+	public void testSetAndGetDIContainer() {
+		DIContainer container = new NullDIContainer();
+		DIContainerFactory.setDIContainer(container);
+		assertTrue("instance unmuch.", container == DIContainerFactory.getDIContainer());
+	}
+	
+	
+	public static class NullDIContainer implements DIContainer {
+
+		public Object getObject(Object key) {
+			return null;
+		}
+
+		public Object getSource() {
+			return null;
+		}
+		
+		@SuppressWarnings("unchecked")
+		public <T> T getObject(Class<T> type, Object key) {
+			return (T)getObject(key);
+		}
+		
+		@SuppressWarnings("unchecked")
+		public <T> T getSource(Class<T> type) {
+			return (T)getSource();
+		}
+	}
+}
