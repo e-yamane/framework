30df3bd03f88e79caec075ec09c68957304540b3
e-yamane <e-yamane@32b2b61f-7f20-47f8-a8dc-572efa4bece0>
2009-07-07 14:38:46 +0000

    git-svn-id: https://dev.rough-diamond.jp/svn/framework/framework/trunk/framework@662 32b2b61f-7f20-47f8-a8dc-572efa4bece0

---
+++ src/java/jp/rough_diamond/tools/TestDataEraser.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2008, 2009
+ *  Rough Diamond Co., Ltd.              -- http://www.rough-diamond.co.jp/
+ *  Information Systems Institute, Ltd.  -- http://www.isken.co.jp/
+ *  All rights reserved.
+ */
+
+package jp.rough_diamond.tools;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import jp.rough_diamond.framework.service.Service;
+import jp.rough_diamond.framework.service.ServiceLocator;
+import jp.rough_diamond.framework.transaction.ConnectionManager;
+
+/**
+ *
+ */
+public class TestDataEraser implements Service {
+	public static void main(String[] args) throws Exception {
+		ServiceLocator.getService(TestDataEraser.class).doIt();
+	}
+
+	public void doIt() throws Exception {
+		Connection con = ConnectionManager.getConnectionManager().getCurrentConnection(null);
+		Statement stmt = con.createStatement();
+		try {
+			ResultSet rs = stmt.executeQuery("select test_table from test_data");
+			while(rs.next()) {
+				dropTable(con, rs.getString(1));
+			}
+			dropTable(con, "test_data");
+		} finally {
+			stmt.close();
+		}
+	}
+	
+	void dropTable(Connection con, String name) throws Exception {
+		System.out.println(name + "ÇçÌèúÇµÇ‹Ç∑ÅB");
+		Statement stmt = con.createStatement();
+		try {
+			stmt.execute("drop table " + name);
+		} finally {
+			stmt.close();
+		}
+	}
+}
